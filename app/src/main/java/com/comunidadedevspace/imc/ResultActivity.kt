f6115c4d2d5d2f1cdb065a08c06bafd55f88c1f3
Ana Paula Lacerda <a-lacerda98@hotmail.com>
2025-03-30 17:41:13 -0300

    RESULTADO DE CLASSIFICACAO NA TELA

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -16,8 +16,22 @@ class ResultActivity : AppCompatActivity() {
         val result = intent.getFloatExtra("KEY_RESULT_IMC", 0f)
         val tvResult = findViewById<TextView>(R.id.tv_result)
         val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao)
-
         tvResult.text = result.toString()
+
+        val classificacao: String = if (result <= 18.5f) {
+           "MAGREZA"
+        }else if(result > 18.5f && result <= 29.9f) {
+         "NORMAL"
+        }else if(result > 25f && result <= 29.9f) {
+            "SOBREPESO"
+        }else if(result > 30f && result <= 39.9f) {
+            "OBESIDADE"
+        }else {
+            "OBESIDADE GRAVE"
+
+        }
+
+        tvClassificacao.text = classificacao
 
 
 
